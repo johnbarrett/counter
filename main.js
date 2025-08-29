828a07e91fdf99b4738f10c06502cfa81ecf01e0
John Barrett <johnbarr@hawaii.edu>
2025-08-29 11:26:55 -1000

    initial commit
    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+var counter = 0;
+
+function add(){
+
+    counter++;
+    document.getElementById("counter").innerHTML = counter;
+}
+
+function sub(){
+
+    counter--
+    document.getElementById("counter").innerHTML = counter;
+
+ 
+}
+
